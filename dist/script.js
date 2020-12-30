aa558bbdb1eed14f67d50219812af4332ed4974a
Vithor <vithort@gmail.com>
2020-12-30 11:58:10 -0300

    Separando Estado e Render
    Separando Estado e Render

--- dist/script.js
+++ dist/script.js
@@ -5,20 +5,33 @@ window.onload = () => {
 class JogoVelha {
   constructor() {
     this.iniciaElementos();
-    this.turno = true;
+    this.iniciaEstado();
   }
 
   iniciaElementos() {
     this.velha = document.querySelector('#velha');
-    this.velha.addEventListener('click', this.realizaJogada.bind(this));
+    this.velha.addEventListener('click', (event) => {
+      this.realizaJogada(event);
+      this.render();
+    });
+  }
+
+  iniciaEstado() {
+    this.turno = true;
+    this.jogadas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   }
 
   realizaJogada(event) {
-    event.target.innerHTML = this.turno ? 'X' : 'O';
+    const id = event.target.dataset.id;
+    // event.target.innerHTML = this.turno ? 'X' : 'O';
+    this.jogadas[id] = this.turno ? 'X' : 'O';
     this.turno = !this.turno;
   }
 
   render() {
-    
+    const velhaElemento = document.querySelectorAll('[data-id]');
+    for (let i = 0; i < 9; i++) {
+      velhaElemento[i].innerHTML = this.jogadas[i] == 0 ? '' : this.jogadas[i];
+    }
   }
 }
